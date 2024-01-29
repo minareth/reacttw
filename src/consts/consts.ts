8e2bf602cfe948d92aeddadda0841b4705ae38ab
minareth <tekarimegraesh@gmail.com>
2024-01-29 13:43:26 +0200

    202401291343

---
+++ src/consts/consts.ts
@@ -0,0 +1,18 @@
+export const stats_primary_dictionary = {
+  constitution: "",
+  endurance: "",
+  dexterity: "",
+  psyche: "",
+  intelligence: "",
+  charisma: "",
+  memory: "",
+}
+
+export const stats_secondary_dictionary = {
+  health: "",
+  stability: "",
+  mana: "",
+  money: "",
+  atk: "",
+  def: "",
+}
